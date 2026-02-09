ea1ba7e9bcfb0eb107c4dfe7f4ad9248f924e7a4
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-09 20:02:15 +0000

    chore: update dependencies and improve TypeScript configuration
    - Added new dependencies: @apollo/server, @as-integrations/express5, @nestjs/apollo, @nestjs/graphql, bcryptjs, class-transformer, class-validator, graphql, and @types/bcryptjs.
    - Updated existing dependencies to their latest versions.
    - Refined TypeScript configuration by cleaning up the paths and ensuring proper library inclusion.

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,9 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
-  providers: [],
 })
 export class AppModule {}
---
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Prisma } from '@generated/prisma-jobber-auth';
+import { Injectable } from '@nestjs/common';
+import { hash } from 'bcryptjs';
+import { PrismaService } from '../prisma/prisma.service';
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10),
+      },
+    });
+  }
+
+  async getUsers() {
+    return this.prismaService.user.findMany();
+  }
+}
