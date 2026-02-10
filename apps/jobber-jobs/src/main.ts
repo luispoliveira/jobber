8744615b68bef2a145ca384097852b82ad661b26
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-10 10:52:49 +0000

    feat: add jobber-jobs application with initial configuration and setup

---
+++ apps/jobber-jobs/src/main.ts
@@ -0,0 +1,30 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const globalPrefix = 'api';
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.setGlobalPrefix(globalPrefix);
+  app.use(cookieParser());
+  const port = configService.getOrThrow<number>('PORT');
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+
+bootstrap();
