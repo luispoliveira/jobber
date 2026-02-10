4327a7b25c003ada059e886bc4689f0cbd6e8474
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-10 22:36:38 +0000

    feat: implement Fibonacci data handling and serialization in Pulsar consumer

--- libs/pulsar/src/lib/pulsar.consumer.ts
+++ libs/pulsar/src/lib/pulsar.consumer.ts
@@ -1,9 +1,12 @@
-import { OnModuleInit } from '@nestjs/common';
+import { Logger, OnModuleInit } from '@nestjs/common';
 import { Consumer, Message } from 'pulsar-client';
 import { PulsarClient } from './pulsar.client';
+import { deserialize } from './serialize';
 
-export abstract class PulsarConsumer implements OnModuleInit {
+export abstract class PulsarConsumer<T> implements OnModuleInit {
   private consumer!: Consumer;
+
+  protected readonly logger = new Logger(this.topic);
 
   constructor(
     private readonly pulsarClient: PulsarClient,
@@ -13,13 +16,27 @@ export abstract class PulsarConsumer implements OnModuleInit {
   async onModuleInit() {
     this.consumer = await this.pulsarClient.createConsumer(
       this.topic,
-      this.onMessage.bind(this),
+      this.listener.bind(this),
     );
   }
 
-  protected async acknowledge(message: Message) {
-    await this.consumer.acknowledge(message);
+  private async listener(message: Message) {
+    try {
+      const data = deserialize<T>(message.getData());
+
+      this.logger.debug(
+        `Received message on topic ${this.topic}: ${JSON.stringify(data)}`,
+      );
+      await this.onMessage(data);
+    } catch (error) {
+      this.logger.error(
+        `Error processing message on topic ${this.topic}`,
+        error,
+      );
+    } finally {
+      await this.consumer.acknowledge(message);
+    }
   }
 
-  protected abstract onMessage(message: Message): Promise<void>;
+  protected abstract onMessage(data: T): Promise<void>;
 }
