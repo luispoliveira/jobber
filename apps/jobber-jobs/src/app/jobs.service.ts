e77e1ebf4422f51c51f7895dc6d80a064c2ad914
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-10 22:10:34 +0000

    feat: enhance ExecuteJobInput to include data field and update executeJob method to handle it

--- apps/jobber-jobs/src/app/jobs.service.ts
+++ apps/jobber-jobs/src/app/jobs.service.ts
@@ -29,7 +29,7 @@ export class JobsService implements OnModuleInit {
     return this.jobs.map((job) => job.meta);
   }
 
-  async executeJob(name: string) {
+  async executeJob(name: string, data: object) {
     const job = this.jobs.find((job) => job.meta.name === name);
     if (!job) {
       throw new BadRequestException(`Job with name ${name} not found`);
@@ -41,7 +41,7 @@ export class JobsService implements OnModuleInit {
       );
     }
 
-    await job.discoveredClass.instance.execute({}, job.meta.name);
+    await job.discoveredClass.instance.execute(data, job.meta.name);
     return job.meta;
   }
 }
