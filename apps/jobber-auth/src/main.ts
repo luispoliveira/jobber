274ad8d44348a3529761385ed93f2a4305747f8d
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-10 14:03:11 +0000

    feat: add gRPC microservice configuration and update build target defaults for proto files

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -2,11 +2,13 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+import { AUTH_PACKAGE_NAME } from '@generated/proto-auth';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
 import cookieParser from 'cookie-parser';
+import { join } from 'node:path';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -23,6 +25,17 @@ async function bootstrap() {
   app.use(cookieParser());
 
   const port = configService.getOrThrow<number>('PORT');
+
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(port);
 
   Logger.log(
