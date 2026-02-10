8192f23276b067b60a557f9afa657be222cfa9f8
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-10 10:43:07 +0000

    feat: add CurrentUser decorator for retrieving authenticated user in GraphQL queries

--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -1,6 +1,8 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from '../auth/current-user.decorator';
 import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { TokenPayload } from '../auth/token-payload.interface';
 import { CreateUserInput } from './dto/create-user.input';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
@@ -16,7 +18,8 @@ export class UsersResolver {
 
   @UseGuards(GqlAuthGuard)
   @Query(() => [User], { name: 'users' })
-  async getUsers() {
+  async getUsers(@CurrentUser() user: TokenPayload) {
+    console.log('Current user:', user);
     return this.usersService.getUsers();
   }
 }
