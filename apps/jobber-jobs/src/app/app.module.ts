c91452efd16c6946e5dfd31bd4ef9a6c2ebd0c82
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-10 12:50:15 +0000

    feat: integrate GraphQL with job execution capabilities and add job model

--- apps/jobber-jobs/src/app/app.module.ts
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -1,8 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [
+    ConfigModule,
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+      context: ({ req, res }) => ({ req, res }),
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
