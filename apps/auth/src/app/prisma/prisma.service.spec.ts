866eceb08ef2aa0e36e553ab9ea47cd4a8aefce9
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-12 10:52:14 +0000

    feat: add jobs application with GraphQL support and job execution functionality
    - Created a new `jobs` application with NestJS framework.
    - Implemented job execution logic using Pulsar for message handling.
    - Added decorators and interfaces for job metadata.
    - Introduced GraphQL resolvers and input types for job execution.
    - Configured TypeScript and Webpack for the new application.
    - Removed obsolete e2e tests and configurations for jobber-auth and jobber-executor applications.
    - Updated package.json scripts for starting the application.
    - Adjusted tsconfig.base.json for improved path mappings.

---
+++ apps/auth/src/app/prisma/prisma.service.spec.ts
@@ -0,0 +1,31 @@
+import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from './prisma.service';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PrismaService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: jest
+              .fn()
+              .mockReturnValue(
+                'postgresql://user:password@localhost:5432/auth',
+              ),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ apps/auth/src/app/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@generated/prisma-auth';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: configService.getOrThrow<string>('DATABASE_URL'),
+    });
+    super({
+      adapter,
+    });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
