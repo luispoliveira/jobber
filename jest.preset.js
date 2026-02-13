6cb6b9642a0c8034f2906aea6936bf0b6a33000e
Luis Oliveira <luispedroliveira96@gmail.com>
2026-02-13 10:32:58 +0000

    feat: update CI configuration to include AWS credentials and standardize formatting in jest configuration files

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,7 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset, passWithNoTests: true };
+module.exports = {
+  ...nxPreset,
+  passWithNoTests: true,
+  transformIgnorePatterns: ['node_modules/(?!uuid/)'],
+};
